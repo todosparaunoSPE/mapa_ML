2fba843347ad057b652ea296cafba1a6651fd132
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-11 12:04:04 -0600

    Update mapac5.py

--- mapac5.py
+++ mapac5.py
@@ -12,7 +12,21 @@ import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
 
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Cambiar el fondo de la página a blanco
 st.markdown("""
